a040a380eb207b58e92f356af39869e11a46edb5
scott chiang <scott01272001@gmail.com>
2023-06-14 23:54:05 +0800

    add discovery service

---
+++ discovery-server/src/test/java/com/scott/discoveryserver/DiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scott.discoveryserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
