9b1081affbb5158af81d0bec59195df74a76bea7
scott chiang <scott01272001@gmail.com>
2023-06-19 23:19:30 +0800

    add keycloak

---
+++ api-gateway/src/main/java/com/scott/apigateway/config/SecurityConfig.java
@@ -0,0 +1,34 @@
+package com.scott.apigateway.config;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+import static org.springframework.security.web.server.util.matcher.ServerWebExchangeMatchers.anyExchange;
+
+import java.util.List;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@EnableWebFluxSecurity
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityWebFilterChain SecurityWebFilterChain(ServerHttpSecurity http) {
+        http.csrf((csrf) ->
+            csrf.disable()
+        );
+
+       // http.authorizeExchange(ex -> ex.anyExchange().permitAll());
+
+        http.authorizeExchange(exchange ->
+            exchange.pathMatchers("/eureka/**").permitAll()
+                .anyExchange().authenticated())
+            .oauth2ResourceServer(oauth2 -> oauth2
+                .jwt(withDefaults()));
+
+        return http.build();
+    }
+
+}
--- readme.md
+++ readme.md
@@ -2,3 +2,6 @@
 
 ### run db
 docker-compose -f ./docker-compose.yml up -d
+
+### keyclock
+docker run -p 8010:8080 -e KEYCLOAK_ADMIN=admin -e KEYCLOAK_ADMIN_PASSWORD=admin --name keycloak quay.io/keycloak/keycloak:21.1.1 start-dev
